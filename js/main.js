b806f8b72e01c6e18bd4a5496580dd51578366eb
Sergio Elisondo <sergioelisondo@gmail.com>
2021-02-10 00:45:09 -0800

    this is a background color selector

---
+++ js/main.js
@@ -0,0 +1,90 @@
+document.getElementById('purple').onclick = partyPurple
+document.getElementById('green').onclick = partyGreen
+document.getElementById('blue').onclick = partyBlue
+document.getElementById('crimson').onclick = deepCrimson
+document.getElementById('wornPurple').onclick = wornPurple
+document.getElementById('navyBlue').onclick = midBlue
+document.getElementById('cherryRed').onclick = cherryRed
+document.getElementById('forestGreen').onclick = forestGreen
+document.getElementById('orange').onclick = orange
+document.getElementById('gray').onclick = gray
+document.getElementById('graphiteBlack').onclick = graphiteBlack
+document.getElementById('blueGray').onclick = blueGray
+document.getElementById('dirtyWhite').onclick = dirtyWhite
+document.querySelector('#skyblue').onclick = skyblue
+document.getElementById('hotPink').onclick = hotPink
+
+function partyPurple() {
+  document.querySelector('body').style.backgroundColor = 'rgb(169, 53, 173)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function partyGreen() {
+  document.querySelector('body').style.backgroundColor = 'rgb(11, 146, 22)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function partyBlue() {
+  document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function deepCrimson() {
+  document.querySelector('body').style.backgroundColor = 'rgb(58, 5, 5)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function wornPurple() {
+  document.querySelector('body').style.backgroundColor = 'rgb(58, 38, 75)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function midBlue() {
+  document.querySelector('body').style.backgroundColor = 'rgb(5, 5, 109)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function cherryRed() {
+  document.querySelector('body').style.backgroundColor = 'rgb(150, 4, 4)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function forestGreen() {
+  document.querySelector('body').style.backgroundColor = 'rgb(3, 37, 3)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function orange() {
+  document.querySelector('body').style.backgroundColor = 'rgb(255, 94, 0)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function gray() {
+  document.querySelector('body').style.backgroundColor = 'rgb(77, 76, 76)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function graphiteBlack() {
+  document.querySelector('body').style.backgroundColor = 'rgb(12, 12, 12)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function blueGray() {
+  document.querySelector('body').style.backgroundColor = 'rgb(43, 77, 150)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function dirtyWhite() {
+  document.querySelector('body').style.backgroundColor = 'rgb(241, 236, 204)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function skyblue () {
+  document.querySelector('body').style.backgroundColor = 'rgb(100, 156, 219)'
+  document.querySelector('body').style.color = "white"
+}
+
+function hotPink () {
+  document.querySelector('body').style.backgroundColor = 'rgb(255, 48, 210)'
+  document.querySelector('body').style.color = 'white'
+}
